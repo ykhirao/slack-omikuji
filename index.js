93e251d0dbe31f3524e6c66ca98a14bccf3b1448
Yuki <yuki.dees39th@gmail.com>
2020-01-16 21:51:44 +0900

    add prettier

--- index.js
+++ index.js
@@ -1,44 +1,58 @@
-require("dotenv").config();
+require('dotenv').config()
 
-const { App } = require("@slack/bolt");
+const { App } = require('@slack/bolt')
 // Initializes your app with your bot token and signing secret
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET
-});
+})
 
-app.message("hello", ({ message, say }) => {
+app.message('hello', ({ message, say }) => {
   // say() sends a message to the channel where the event was triggered
   say({
     blocks: [
       {
-        type: "section",
+        type: 'section',
         text: {
-          type: "mrkdwn",
+          type: 'mrkdwn',
           text: `Hey there <@${message.user}>!`
         },
         accessory: {
-          type: "button",
+          type: 'button',
           text: {
-            type: "plain_text",
-            text: "Click Me"
+            type: 'plain_text',
+            text: 'Click Me'
           },
-          action_id: "button_click"
+          action_id: 'button_click'
         }
       }
     ]
-  });
-});
+  })
+})
 
-app.action("button_click", ({ body, ack, say }) => {
+app.action('button_click', ({ body, ack, say }) => {
   // Acknowledge the action
-  ack();
-  say(`<@${body.user.id}> clicked the button`);
-});
+  ack()
+  say(`<@${body.user.id}> clicked the button`)
+})
 
-(async () => {
+app.message('wake me up', async ({ message, context }) => {
+  try {
+    // トークンを用いて chat.scheduleMessage 関数を呼び出す
+    const result = await app.client.chat.scheduleMessage({
+      // アプリの初期化に用いたトークンを `context` オブジェクトに保存
+      token: context.botToken,
+      channel: message.channel.id,
+      post_at: whenSeptemberEnds,
+      text: 'Summer has come and passed'
+    })
+  } catch (error) {
+    console.error(error)
+  }
+})
+;(async () => {
   // Start your app
-  await app.start(process.env.PORT || 3000);
+  await app.start(process.env.PORT || 3000)
 
-  console.log("⚡️ Bolt app is running!");
-})();
+  console.log('⚡️ Bolt app is running!')
+})()
