65405d90452ca2cc9b57a45b0477c5b617be0118
Yuki <yuki.dees39th@gmail.com>
2020-01-16 22:31:12 +0900

    update

--- index.js
+++ index.js
@@ -1,54 +1,25 @@
 require('dotenv').config()
 
 const { App } = require('@slack/bolt')
-// Initializes your app with your bot token and signing secret
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET
 })
 
-app.message('hello', ({ message, say }) => {
-  // say() sends a message to the channel where the event was triggered
-  say({
-    blocks: [
-      {
-        type: 'section',
-        text: {
-          type: 'mrkdwn',
-          text: `Hey there <@${message.user}>!`
-        },
-        accessory: {
-          type: 'button',
-          text: {
-            type: 'plain_text',
-            text: 'Click Me'
-          },
-          action_id: 'button_click'
-        }
-      }
-    ]
-  })
-})
-
-app.action('button_click', ({ body, ack, say }) => {
-  // Acknowledge the action
+app.command('/omikuji', async ({ message, context, ack, say }) => {
   ack()
-  say(`<@${body.user.id}> clicked the button`)
-})
 
-app.message('wake me up', async ({ message, context }) => {
-  try {
-    // トークンを用いて chat.scheduleMessage 関数を呼び出す
-    const result = await app.client.chat.scheduleMessage({
-      // アプリの初期化に用いたトークンを `context` オブジェクトに保存
-      token: context.botToken,
-      channel: message.channel.id,
-      post_at: whenSeptemberEnds,
-      text: 'Summer has come and passed'
+  const result = await app.client.usergroups
+    .list({
+      token: process.env.SLACK_USER_TOKEN,
+      include_users: true
     })
-  } catch (error) {
-    console.error(error)
+    .catch(error => console.log(error))
+  if (!result.ok) {
+    console.log(`OK FALSE: ${result.error}`)
   }
+  console.log(result.usergroups)
+  say('finish')
 })
 ;(async () => {
   // Start your app
