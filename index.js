48c66153784db783150b331bd7441b2a6c5d7799
Yuki <yuki.dees39th@gmail.com>
2020-01-16 21:44:16 +0900

    update

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
-const { App } = require("@slack/bolt");
+require("dotenv").config();
 
+const { App } = require("@slack/bolt");
 // Initializes your app with your bot token and signing secret
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+app.message("hello", ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  say({
+    blocks: [
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: `Hey there <@${message.user}>!`
+        },
+        accessory: {
+          type: "button",
+          text: {
+            type: "plain_text",
+            text: "Click Me"
+          },
+          action_id: "button_click"
+        }
+      }
+    ]
+  });
+});
+
+app.action("button_click", ({ body, ack, say }) => {
+  // Acknowledge the action
+  ack();
+  say(`<@${body.user.id}> clicked the button`);
 });
 
 (async () => {
